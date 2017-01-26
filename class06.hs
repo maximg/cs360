22f3cf9c53922c368bba22052220c41ae3922f89
Maksim Golov <m.golov@activevideo.com>
2017-01-26 07:43:53 +0100

    Implemented parsePostfix and eval

--- class06.hs
+++ class06.hs
@@ -2,6 +2,24 @@
 
 import Data.Char   -- for isSpace, isDigit
 
+data Op where
+  Plus  :: Op
+  Minus :: Op
+  Times :: Op
+  deriving (Show, Eq)
+
+data Arith where
+  Lit :: Integer -> Arith
+  Bin :: Op -> Arith -> Arith -> Arith
+  deriving (Show)
+
+interpArith :: Arith -> Integer
+interpArith (Lit x) = x
+interpArith (Bin Plus  l r) = interpArith l + interpArith r
+interpArith (Bin Minus l r) = interpArith l - interpArith r
+interpArith (Bin Times l r) = interpArith l * interpArith r
+
+
 data Token where
   TLit   :: Integer -> Token
   TPlus  :: Token
@@ -23,6 +41,7 @@ tokenize (' ' : xs) = tokenize xs
 tokenize xs         = let (lit,rest) = span isDigit xs
                       in TLit (read lit) : (tokenize rest)
 
+-- FIXME: swap the order of arguments for helper
 shunt :: [Token] -> [Token]
 shunt xs = helper xs [] where
     helper [] tmp = tmp
@@ -32,14 +51,25 @@ shunt xs = helper xs [] where
     helper (RParen : xs) (x : rest)      = x : (helper (RParen : xs) rest)
     helper (RParen : xs) []              = undefined
     helper (op     : xs) []              = helper xs [op]
-    --helper (op     : xs) (LParen : rest) = helper xs (op:LParen:rest)
     helper (op     : xs) (op' : rest) | (prec op) <= (prec op') = op' : (helper (op:xs) rest)
                                       | otherwise = helper xs (op:op':rest)
+    prec TTimes = 2
+    prec TPlus  = 1
+    prec TMinus = 1
+    prec LParen = 0
+    prec RParen = 0
+    prec _      = undefined
 
-prec :: Token -> Int
-prec TTimes = 2
-prec TPlus  = 1
-prec TMinus = 1
-prec LParen = 0
-prec RParen = 0
-prec _      = undefined
+
+parsePostfix :: [Token] -> Arith
+parsePostfix = head . helper [] where
+    helper :: [Arith] -> [Token] -> [Arith]
+    helper out [] = out
+    helper out ((TLit x):xs) = helper ((Lit x):out) xs
+    helper (r:l:out) (op:xs) = helper ((mkOp op):out) xs where
+        mkOp TPlus  = Bin Plus l r
+        mkOp TMinus = Bin Minus l r
+        mkOp TTimes = Bin Times l r
+
+eval :: String -> Integer
+eval = interpArith . parsePostfix . shunt . tokenize
