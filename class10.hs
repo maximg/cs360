814dd055df7cc70271fce1cccdaa194ef6fbd08c
Maksim Golov <m.golov@activevideo.com>
2017-01-29 08:24:44 +0100

    Implemented interpArith

--- class10.hs
+++ class10.hs
@@ -28,23 +28,25 @@ data Type where
     TInteger :: Type
     deriving (Show, Eq)
 
-data Value where
-    VInt :: Integer -> Value
-    VBool :: Bool -> Value
-    deriving (Show, Eq)
-
-type Env = M.Map String Value
+type Env = M.Map String Integer
 type Ctx = M.Map String Type
 
 data TypeError where
     UndefinedVar :: String -> TypeError
     -- expr, expected type, actual type
     TypeMismatch :: Arith -> Type -> Type -> TypeError
+    -- if expr, type then, type else
+    IfBranchesMismatch :: Arith -> Type -> Type -> TypeError
     deriving (Show)
 
 showTypeError :: TypeError -> String
 showTypeError (UndefinedVar v) = "Undefined variable '" ++ v ++ "'"
-showTypeError (TypeMismatch e t1 t2) = "Type mismatch .........."
+showTypeError (TypeMismatch e t1 t2) =
+    "Type mismatch: expected " ++ show t1 ++
+    " but got " ++ show t2 ++ " in expression " ++ showArith e
+showTypeError (IfBranchesMismatch e t1 t2) =
+    "Branches have different types: " ++ show t1 ++
+    " and " ++ show t2 ++ " in expression " ++ showArith e
 
 data InterpError where
     DivisonByZero :: InterpError
@@ -52,6 +54,28 @@ data InterpError where
 
 showInterpError :: InterpError -> String
 showInterpError (DivisonByZero)  = "Division by zero"
+
+
+showOp :: Op -> String
+showOp Plus  = "+"
+showOp Minus = "-"
+showOp Times = "*"
+showOp Div   = "/"
+showOp Less  = "<"
+showOp Eq   = "=="
+
+
+showArith :: Arith -> String
+showArith (Lit i) = show i
+showArith BTrue = "True"
+showArith BFalse = "False"
+showArith (Bin op e1 e2) = (showArith e1) ++ showOp op ++ (showArith e2)
+showArith (Var x) = x
+showArith (Let v e1 e2) = "let " ++ v ++ " = " ++ showArith e1 ++ " in " ++ (showArith e2)
+showArith (If cond eThen eElse) =
+    "if " ++ showArith cond ++
+    " then " ++ showArith eThen ++
+    " else " ++ showArith eElse
 
 
 bopType :: Op -> (Type, Type, Type)
@@ -75,11 +99,11 @@ infer ctx (Bin op e1 e2) = do
 infer ctx (Var v) = case M.lookup v ctx of
                         Just x -> Right x
                         Nothing -> Left $ UndefinedVar v
-infer ctx (If cond eThen eElse) = do
+infer ctx e@(If cond eThen eElse) = do
     check ctx cond TBool
     t1 <- infer ctx eThen
     t2 <- infer ctx eElse
-    if t1 /= t2 then Left $ TypeMismatch eElse t1 t2
+    if t1 /= t2 then Left $ IfBranchesMismatch e t1 t2
                 else Right t1
 infer ctx (Let name val expr) = do
     t <- infer ctx val
@@ -94,6 +118,36 @@ check ctx e t = do
 
 inferArith :: Arith -> Either TypeError Type
 inferArith = infer M.empty
+
+
+interpArith :: Env -> Arith -> Either InterpError Integer
+interpArith _ (Lit i) = Right i
+interpArith _ BTrue = Right 1
+interpArith _ BFalse = Right 0
+
+interpArith env (Bin Plus  e1 e2) = (+) <$> interpArith env e1 <*> interpArith env e2
+interpArith env (Bin Minus e1 e2) = (-) <$> interpArith env e1 <*> interpArith env e2
+interpArith env (Bin Times e1 e2) = (*) <$> interpArith env e1 <*> interpArith env e2
+interpArith env (Bin Div   e1 e2) =
+    (interpArith env e2 >>= div') <*> interpArith env e1
+    where
+        div' 0 = Left DivisonByZero
+        div' x = Right (\y -> x `div` y)
+
+interpArith env (Bin Less e1 e2) = less <$> interpArith env e1 <*> interpArith env e2
+    where less x y = if x < y then 1 else 0
+interpArith env (Bin Eq   e1 e2) = eq <$> interpArith env e1 <*> interpArith env e2
+    where eq x y = if x == y then 1 else 0
+
+interpArith env (Var v) = case M.lookup v env of
+                            Just x -> Right x
+                            Nothing -> error "Bug: undefined var not caught by type check"
+interpArith env (Let name val expr) =
+    interpArith env val >>= (\x -> interpArith (M.insert name x env) expr)
+interpArith env (If cond eTrue eFalse) = do
+    cond' <- interpArith env cond
+    if cond' == 1 then interpArith env eTrue
+                  else interpArith env eFalse
 
 
 lexer :: TokenParser u
@@ -178,11 +232,18 @@ parseArith = buildExpressionParser table parseArithAtom
 arith :: Parser Arith
 arith = whiteSpace *> parseArith <* eof
 
-{-
+
+showAsType :: Type -> Integer -> String
+showAsType TBool 0 = "False"
+showAsType TBool 1 = "True"
+showAsType _ x = show x
+
+
 eval :: String -> IO ()
 eval s = case parse arith s of
-  Left err  -> print err
-  Right e -> case interpArith3 M.empty e of
-    Left err -> putStrLn (showInterpError err)
-    Right val -> print val
--}
+    Left err  -> print err
+    Right e -> case inferArith e of
+        Left err -> putStrLn (showTypeError err)
+        Right t -> case interpArith M.empty e of
+            Left err -> putStrLn (showInterpError err)
+            Right val -> putStrLn $ showAsType t val
